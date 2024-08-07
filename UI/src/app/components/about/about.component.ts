4cb522ca387d2b300aa509ce8ddfa8e66338d791
jsm33t <jskainthofficial@gmail.com>
2024-08-07 23:49:07 +0530

    about_page

--- UI/src/app/components/about/about.component.ts
+++ UI/src/app/components/about/about.component.ts
@@ -1,14 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent implements OnInit {
+
+  socialLinks = [
+    {
+        platform: 'Instagram',
+        icon: 'ai-instagram',
+        url: 'https://instagram.com/jsm33t',
+    },
+    {
+        platform: 'Facebook',
+        icon: 'ai-facebook',
+        url: 'https://facebook.com/iamjsm33t',
+    },
+    {
+        platform: 'YouTube',
+        icon: 'ai-github',
+        url: 'https://github.com/jsm33t',
+    },
+];
+
   constructor(private titleService: Title, private metaService: Meta) { }
 
   ngOnInit(): void {
--- UI/src/app/components/home/home.component.ts
+++ UI/src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { initParallax } from '../../library/invokers/parallax';
 import InitAnimateOnScroll from '../../library/invokers/animate-on-scroll';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
