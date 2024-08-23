f8c3f36618c917645ef9966e65d3f6a7f38bd8c5
jsm33t <jskainthofficial@gmail.com>
2024-08-23 10:06:58 +0530

    :art: remove vercel analytics and brokwn links form index.html

--- UI/src/app/app.component.ts
+++ UI/src/app/app.component.ts
@@ -6,8 +6,8 @@ import { SidePanelComponent } from './components/shared/side-panel/side-panel.co
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { BackToTopComponent } from './components/shared/back-to-top/back-to-top.component';
-import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
+// import { inject } from '@vercel/analytics';
+// import { injectSpeedInsights } from '@vercel/speed-insights';
 import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
 import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 import setTheme from './library/invokers/settheme';
@@ -39,12 +39,10 @@ export class AppComponent implements OnInit{
 
 		this.completeLoading();
 		initBackToTop();
-		inject();
-		injectSpeedInsights();
+		// inject();
+		// injectSpeedInsights();
 		setInterval(rotateText, 100);
 	}
-
-
 
 	completeLoading() {
 		setTimeout(function () {
